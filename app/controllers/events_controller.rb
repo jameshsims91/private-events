85fc2417347b28d57c0f251a917413b5d75a7e00
jameshsims91 <jameshsims91@gmail.com>
2026-06-24 12:29:50 -0400

    Work on app

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,30 @@
 class EventsController < ApplicationController
-  before_action :protect_from_forgery, :authenticate_user!, except: [ :index, :show ]
+  before_action :protect_from_forgery, :authenticate_user!, except: [ :index, :new, :create ]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.buld(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :title, :date, :location, :description ])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_24_142355) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_24_161231) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.integer "creator_id", null: false
     t.datetime "date"
+    t.text "description"
     t.string "location"
     t.integer "min_age"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
@@ -31,4 +35,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_24_142355) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
